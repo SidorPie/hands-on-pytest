db43fb752b46ed0295616a7fee59d0149b9b8afc
SidorPie <67256690+SidorPie@users.noreply.github.com>
2024-08-28 16:32:25 +0300

    Add login page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,20 +1,20 @@
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-import time
-
-link = "http://selenium1py.pythonanywhere.com/"
+from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
 
-def test_add_to_cart(browser):
-    page = ProductPage(browser,url="")  # Инициализируем объект Page Object
-    page.open()                         # Открываем страницу в браузере
-    page.should_be_add_to_cart_button() # Проверяем, что есть кнопка добавления в корзину
-    page.add_product_to_cart()          # Жмем кнопку добавить в корзину 
-    page.should_be_success_message()    # Проверяем, что есть сообщение с нужным текстом
+# def test_add_to_cart(browser):
+#     page = ProductPage(browser,url="")  # Инициализируем объект Page Object
+#     page.open()                         # Открываем страницу в браузере
+#     page.should_be_add_to_cart_button() # Проверяем, что есть кнопка добавления в корзину
+#     page.add_product_to_cart()          # Жмем кнопку добавить в корзину 
+#     page.should_be_success_message()    # Проверяем, что есть сообщение с нужным текстом
